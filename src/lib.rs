8ad5353c3364fd88b8b764599fa9f57de2e8e455
Yu212 <yu212.mc@gmail.com>
2024-04-04 22:05:57 +0900

    [add] .llのファイル出力と最適化

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use std::mem;
+use std::path::PathBuf;
 
 use inkwell::context::Context;
+use inkwell::module::Module;
 use inkwell::OptimizationLevel;
+use inkwell::passes::{PassManager, PassManagerBuilder};
+use inkwell::targets::{InitializationConfig, Target};
 use rowan::ast::AstNode;
 
 use crate::ast::Root;
@@ -92,4 +96,52 @@ pub fn parse(text: &str) {
     unsafe {
         engine.get_function::<unsafe extern "C" fn() -> i32>("main").unwrap().call();
     }
+    module.print_to_file(PathBuf::from("./target/output.ll")).expect("print_to_file failed");
+    optimize(&module);
+    module.print_to_file(PathBuf::from("./target/output_optimized.ll")).expect("print_to_file failed");
+}
+
+pub fn optimize(module: &Module) {
+    let config = InitializationConfig::default();
+    Target::initialize_native(&config).unwrap();
+    let pass_manager_builder = PassManagerBuilder::create();
+    pass_manager_builder.set_optimization_level(OptimizationLevel::Aggressive);
+    let pass_manager = PassManager::create(());
+    pass_manager.add_promote_memory_to_register_pass();
+    pass_manager.add_always_inliner_pass();
+    pass_manager.add_gvn_pass();
+    pass_manager.add_new_gvn_pass();
+    pass_manager.add_function_attrs_pass();
+    pass_manager.add_constant_merge_pass();
+    pass_manager.add_scalarizer_pass();
+    pass_manager.add_merged_load_store_motion_pass();
+    pass_manager.add_instruction_combining_pass();
+    pass_manager.add_memcpy_optimize_pass();
+    pass_manager.add_partially_inline_lib_calls_pass();
+    pass_manager.add_lower_switch_pass();
+    pass_manager.add_reassociate_pass();
+    pass_manager.add_simplify_lib_calls_pass();
+    pass_manager.add_instruction_simplify_pass();
+    pass_manager.add_function_inlining_pass();
+    pass_manager.add_global_optimizer_pass();
+    pass_manager.add_dead_arg_elimination_pass();
+    pass_manager.add_strip_symbol_pass();
+    pass_manager.add_strip_dead_prototypes_pass();
+    pass_manager.add_internalize_pass(true);
+    pass_manager.add_sccp_pass();
+    pass_manager.add_aggressive_dce_pass();
+    pass_manager.add_global_dce_pass();
+    pass_manager.add_tail_call_elimination_pass();
+    pass_manager.add_basic_alias_analysis_pass();
+    pass_manager.add_licm_pass();
+    pass_manager.add_ind_var_simplify_pass();
+    pass_manager.add_loop_vectorize_pass();
+    pass_manager.add_loop_idiom_pass();
+    pass_manager.add_loop_rotate_pass();
+    pass_manager.add_loop_unroll_pass();
+    pass_manager.add_loop_deletion_pass();
+    pass_manager.add_cfg_simplification_pass();
+    pass_manager.add_verifier_pass();
+    pass_manager.run_on(module);
+    pass_manager_builder.populate_module_pass_manager(&pass_manager);
 }
