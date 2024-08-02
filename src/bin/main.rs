950afe120d85975122ea01c56bc6bfa9041c0410
Yu212 <yu212.mc@gmail.com>
2024-08-02 09:25:29 +0000

    [change] 読み込むファイルの拡張子を.zoiに変更

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@ use std::io::Read;
 use zoisite::compile;
 
 fn main() {
-    let mut file = File::open("./files/main").expect("file not found");
+    let mut file = File::open("./files/main.zoi").expect("file not found");
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).expect("failed to read file");
     compile(buffer.as_str());
